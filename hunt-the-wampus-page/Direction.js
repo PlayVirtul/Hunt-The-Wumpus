c3065710c7cf1fe8f48c0874e91e8c908ed6447b
Kirill Sachkov <playvirtul@gmail.com>
2022-07-27 14:13:39 +0300

    feat: add movement

---
+++ hunt-the-wampus-page/Direction.js
@@ -0,0 +1,6 @@
+export default class Direction {
+    static get up() { return 'up'; };
+    static get down() { return 'down'; };
+    static get right() { return 'right'; };
+    static get left() { return 'left'; };
+}
--- hunt-the-wampus-page/game-objects/room.js
+++ hunt-the-wampus-page/game-objects/room.js
@@ -1,6 +1,22 @@
+import Player from "./player.js";
 
 export default class Room {
-	constructor(gameObject) {
-		this.gameObject = gameObject;
-	}
+    constructor(gameObject) {
+        this.gameObject = gameObject;
+    }
+
+    gameObject = null;
+
+    render() {
+        const roomElement = document.createElement('div');
+        roomElement.classList.add('room');
+
+        if (!this.gameObject === false) {
+            if (this.gameObject instanceof Player) {
+                roomElement.classList.add('player');
+            }
+        }
+
+        return roomElement;
+    }
 }
